ccec33dbcc1bc9d57a7662fdc368347e3d8d1e49
Stephanielebihan <94453807+Stephanielebihan@users.noreply.github.com>
2023-10-29 16:47:15 +0100

    modif

--- portfolio.js
+++ portfolio.js
@@ -1,66 +1,57 @@
+var elements = document.querySelector(".btn-navigation");
+var element = document.querySelectorAll(".barre");
+var elementNav = document.querySelector(".navigation");
+const main = document.getElementById("main");
+const background = document.querySelector(".back-container");
 
-var elements = document.querySelector('.btn-navigation');
-var element = document.querySelectorAll('.barre')
-var elementNav = document.querySelector(".navigation")
-const main = document.getElementById('main')
-const background = document.querySelector('.back-container')
+var x = window.matchMedia("(min-width: 768px)");
 
-var x = window.matchMedia("(min-width: 768px)")
+elements.addEventListener("click", function () {
+  element.forEach((element) => {
+    elements.classList.toggle("black");
+    element.classList.toggle("black");
+    elementNav.classList.toggle("isOpen");
 
-elements.addEventListener('click', function(){
-    element.forEach((element)=>{
-
-        elements.classList.toggle('black')        
-        element.classList.toggle('black')
-        elementNav.classList.toggle('isOpen')
-
-        if(x.matches){
-            main.classList.toggle('marginLeft')
-            background.classList.toggle('marginLeftBack')
-        }
-        
-    
-    })
-})
+    if (x.matches) {
+      main.classList.toggle("marginLeft");
+      background.classList.toggle("marginLeftBack");
+    }
+  });
+});
 
 var mobile = window.matchMedia("(min-width: 768px)");
 
-
 var btn = document.querySelectorAll(".modal-button-large");
 
-var modals = document.querySelectorAll('.modal');
-
+var modals = document.querySelectorAll(".modal");
 
 var spans = document.getElementsByClassName("close");
 
 for (var i = 0; i < btn.length; i++) {
- btn[i].onclick = function(e) {
+  btn[i].onclick = function (e) {
     const modal = document.querySelector(e.target.getAttribute("href"));
-    if(mobile.matches){
-    e.preventDefault();
-    const modal = document.querySelector(e.target.getAttribute("href"));
-    modal.style.display = "block";
-    }
-    else(
-        modal.style.display ="none"
-    )
- }
+    if (mobile.matches) {
+      e.preventDefault();
+      const modal = document.querySelector(e.target.getAttribute("href"));
+      modal.style.display = "block";
+    } else modal.style.display = "none";
+  };
 }
 
 for (var i = 0; i < spans.length; i++) {
- spans[i].onclick = function() {
+  spans[i].onclick = function () {
     for (var index in modals) {
-      if (typeof modals[index].style !== 'undefined') modals[index].style.display = "none";    
+      if (typeof modals[index].style !== "undefined")
+        modals[index].style.display = "none";
     }
- }
+  };
 }
 
-window.onclick= function (e) {
-    if (e.target.classList.contains('modal')) {
-       for(let i in modals){
-        if (typeof modals[i].style !== 'undefined') modals[i].style.display = "none";
-       };
+window.onclick = function (e) {
+  if (e.target.classList.contains("modal")) {
+    for (let i in modals) {
+      if (typeof modals[i].style !== "undefined")
+        modals[i].style.display = "none";
     }
+  }
 };
-
-
